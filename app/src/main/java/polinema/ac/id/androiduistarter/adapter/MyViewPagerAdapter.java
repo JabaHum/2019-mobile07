db1b70a510182965dcf3bfd8cac65aef669a0ac7
JabaHum <jabajnan8@gmail.com>
2020-06-12 16:36:16 +0300

    Initial Commit

---
+++ app/src/main/java/polinema/ac/id/androiduistarter/adapter/MyViewPagerAdapter.java
@@ -0,0 +1,48 @@
+/*
+package polinema.ac.id.androiduistarter.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.viewpager.widget.PagerAdapter;
+
+*/
+/**
+ * View pager adapter
+ *//*
+
+public class MyViewPagerAdapter extends PagerAdapter {
+    private LayoutInflater layoutInflater;
+
+    public MyViewPagerAdapter() {
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view = layoutInflater.inflate(layouts[position], container, false);
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public int getCount() {
+        return layouts.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object obj) {
+        return view == obj;
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        View view = (View) object;
+        container.removeView(view);
+    }
+}*/
