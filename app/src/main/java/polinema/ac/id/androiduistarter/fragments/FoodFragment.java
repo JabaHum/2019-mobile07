86cbd816b003c301c37a7a08b86bf4ea2af94779
JabaHum <jabajnan8@gmail.com>
2020-07-02 16:20:23 +0300

    Separated the Adapter

---
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/FoodFragment.java
@@ -0,0 +1,6 @@
+package polinema.ac.id.androiduistarter.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class FoodFragment extends Fragment {
+}
---
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/HomeFragment.java
@@ -0,0 +1,6 @@
+package polinema.ac.id.androiduistarter.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+}
